cab805dc2fa4939509888d703fdcac2694220ef6
Carlos Granados <granados.carlos91@gmail.com>
2015-12-19 10:21:10 -0500

    Update doc

--- index.php
+++ index.php
@@ -2,4 +2,6 @@
 define('appcore\APP_CONFIG', 'myAppConfig');
 require_once realpath(__DIR__) . '/app/autoload.php';
 
+// We'll run the engine in some fancy
+// hipsterish way here.
 AppEngine::init($_SERVER, $_REQUEST)->run();
